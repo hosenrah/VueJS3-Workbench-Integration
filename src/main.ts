2514027095a1e0360354c7d7ea1929eea769adc4
Andreas Rau <andreas.rau@mercedes-benz.io>
2022-01-24 19:22:05 +0100

    naising

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from "vue";
+import App from "./App.vue";
 
 const app = createApp(App);
 
-app.config.isCustomElement = (tag) => tag === "[/wb-.+/]";
-
-app.mount('#app');
+/** app.config.isCustomElement = (tag) => tag === "[/wb-.+/]"; will be deprecated in Vue 3.3
+ * any element starting with 'ion-' will be recognized as a custom one */
+app.config.compilerOptions.isCustomElement = (tag) =>
+  tag.startsWith("[/wb-.+/]");
+app.mount("#app");
